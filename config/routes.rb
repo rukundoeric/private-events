64f531a278445281d2421eabf1d1f1d6dcb37025
Rukundo Eric <ericrukundo005@gmail.com>
2020-12-02 15:22:28 +0200

    Add models

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'users#index'
+
+  get 'sign_up' => 'users#new', as: 'sign_up'
+  get 'sign_in' => 'sessions#new', as: 'sign_in'
+  get 'sign_out' => 'sessions#destroy', as: 'sign_out'
+
+  resources :users
+  resources :sessions, only: %i[new create destroy]
+end
