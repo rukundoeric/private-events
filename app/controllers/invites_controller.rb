fd361ff02a48b6cc80c01c96e85004321f9076c8
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-12-03 07:10:51 +0200

    Allow users to invite other users to events

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -11,13 +11,13 @@ class InvitesController < ApplicationController
     @invite = Invite.new(event_id: event.id, user_id: user_id)
 
     if @invite.save
-      invite.invited!
+      @invite.invited!
       flash[:notice] = 'INVITATION IS SENT SUCCESSFULLY!'
-      redirect_to users_path(event_id: event.id)
     else
       flash[:alert] = 'SOMETHING WENT WRONG!.'
-      redirect_to event_path(event.id)
     end
+
+    redirect_to invites_path(event_id: event.id)
   end
 
   def destroy
@@ -26,21 +26,24 @@ class InvitesController < ApplicationController
 
     if current_user == event.creator
       invite.destroy
-      redirect_to users_path(event_id: event.id, id: invite.id)
     else
       inite.status = 'invited'
       invite.save
-      redirect_to event_path(event)
     end
+
+    redirect_to invites_path(event_id: event.id)
   end
 
   def update
     event = Event.find(params[:event_id])
     invite = Invite.find_by(event_id: params[:event_id], user_id: current_user.id)
 
-    if invite&.invited
+    if invite&.invited?
       invite.accepted!
       flash[:notice] = 'THANKS FOR ACCEPTING TO ANTEND THIS EVENT!'
+    elsif invite&.accepted?
+      invite.invited!
+      flash[:notice] = 'YOU HAVE WITHDRAWN FROM ATTENDING THIS EVENT!'
     else
       flash[:alert] = 'SORRY, YOU ARE NOT INVITED TO THIS EVENT!'
     end
